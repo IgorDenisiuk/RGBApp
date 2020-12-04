296d68b84aade6867e7db5756b2e7e9cf2f24e35
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-04 16:21:18 +0300

    Add Main Storyboard

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redNumbers: UILabel!
+    @IBOutlet var greenNumbers: UILabel!
+    @IBOutlet var blueNumbers: UILabel!
+    @IBOutlet var sliderR: UISlider!
+    @IBOutlet var sliderG: UISlider!
+    @IBOutlet var sliderB: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSlider(_ sender: UISlider) {
+   
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
